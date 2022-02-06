2a4f8b62a710075ba47555e45e55143d0acd772f
Tim Broddin <tim@wannabes.be>
2022-02-06 17:28:08 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,26 +1,20 @@
 const { withPlausibleProxy } = require("next-plausible");
-const withPWA = require("next-pwa");
 
-module.exports = withPWA(
-  withPlausibleProxy()({
-    reactStrictMode: true,
-    images: {
-      domains: ["media.giphy.com"],
-    },
-    async rewrites() {
-      return [
-        {
-          source: "/",
-          destination: "/speel/6",
-        },
-      ];
-    },
-    experimental: {
-      // Enables the styled-components SWC transform
-      styledComponents: true,
-    },
-    pwa: {
-      dest: "public",
-    },
-  })
-);
+module.exports = withPlausibleProxy()({
+  reactStrictMode: true,
+  images: {
+    domains: ["media.giphy.com"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/speel/6",
+      },
+    ];
+  },
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+});
