e95bd342344c3928895f22a463363d4d5c589a11
Tim Broddin <tim@wannabes.be>
2022-02-06 17:19:26 +0100

    initial work on pwa

--- next.config.js
+++ next.config.js
@@ -1,20 +1,26 @@
 const { withPlausibleProxy } = require("next-plausible");
+const withPWA = require("next-pwa");
 
-module.exports = withPlausibleProxy()({
-  reactStrictMode: true,
-  images: {
-    domains: ["media.giphy.com"],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/speel/6",
-      },
-    ];
-  },
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true,
-  },
-});
+module.exports = withPWA(
+  withPlausibleProxy()({
+    reactStrictMode: true,
+    images: {
+      domains: ["media.giphy.com"],
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/",
+          destination: "/speel/6",
+        },
+      ];
+    },
+    experimental: {
+      // Enables the styled-components SWC transform
+      styledComponents: true,
+    },
+    pwa: {
+      dest: "public",
+    },
+  })
+);
