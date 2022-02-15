fac91f867aeb0af921287f438a1c63a2a874fea8
timbroddin <tim@wannabes.be>
2022-02-15 16:41:19 +0100

    remove prefers-high-contrast

--- styles/globals.js
+++ styles/globals.js
@@ -20,14 +20,6 @@ const GlobalStyle = globalCss({
       "--focus-color": "rgba(128,128,255,0.8)",
       "--keyboard-border-color": "#999",
     },
-    "@media (prefers-contrast: more)": {
-      "--color-good": "var(--nextui-colors-yellow500)",
-      "--color-off": "var(--nextui-colors-blue200)",
-      "--color-level": "var(--nextui-colors-primaryDark)",
-      "--color-level-active": "var(--nextui-colors-secondaryDark)",
-      "--color-icon-left": "var(--nextui-colors-primaryDark)",
-      "--color-icon-right": "var(--nextui-colors-pink100)",
-    },
   },
   "body.colorblind": {
     "--color-good": "var(--nextui-colors-yellow500)",
