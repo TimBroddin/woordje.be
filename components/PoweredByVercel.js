c35bd4e68e04913d8561be2c11ed4b294cb316b4
Tim Broddin <tim@wannabes.be>
2022-03-15 08:43:33 +0100

    fix link

--- components/PoweredByVercel.js
+++ components/PoweredByVercel.js
@@ -1,5 +1,5 @@
 import { Container, styled } from "@nextui-org/react";
-import NextLink from "next/link";
+import Link from "next/link";
 import Image from "next/image";
 
 const PoweredBy = styled("div", {
@@ -11,18 +11,17 @@ const PoweredByVercel = () => {
   return (
     <Container gap={1}>
       <PoweredBy>
-        <NextLink
+        <Link
           href="https://vercel.com/?utm_source=Woordje&utm_campaign=oss"
-          passHref>
-          <a target="_blank" norel noreferer>
-            <Image
-              src={"/images/powered-by-vercel.svg"}
-              alt="Powered by Vercel"
-              width="146"
-              height="30"
-            />
-          </a>
-        </NextLink>
+          rel="noreferrer"
+          target="_blank">
+          <Image
+            src={"/images/powered-by-vercel.svg"}
+            alt="Powered by Vercel"
+            width="146"
+            height="30"
+          />
+        </Link>
       </PoweredBy>
     </Container>
   );
