37f407711b5f5ea7f924154b8274837de1d1b657
timbroddin <tim@wannabes.be>
2022-05-05 11:05:13 +0200

    upgrade to react 18

--- pages/_app.js
+++ pages/_app.js
@@ -26,54 +26,55 @@ const darkTheme = createTheme({
 });
 
 const Gate = ({ children }) => {
-  if (typeof window !== "undefined") {
-    return <PersistGate persistor={persistor}>{children}</PersistGate>;
-  } else {
-    return children;
-  }
+   <PersistGate persistor={persistor}>{children}</PersistGate>;
+
 };
 
 function MyApp({ Component, pageProps }) {
   const translations = useTranslations();
   GlobalStyle();
   return (
-    <LazyMotion features={domAnimation}>
-      <NextThemesProvider
-        defaultTheme="system"
-        attribute="class"
-        value={{
-          light: lightTheme.className,
-          dark: darkTheme.className,
-        }}>
-        <NextUIProvider>
-          <Head>
-            <link
-              rel="alternate"
-              hrefLang={translations.alternate_lang}
-              href={translations.alternate_url}
-            />
-          </Head>
-          <PlausibleProvider domain={translations.plausible}>
-            <Provider store={store}>
-              <>
-                <Pwa />
+      <LazyMotion features={domAnimation}>
+          <NextThemesProvider
+              defaultTheme="system"
+              attribute="class"
+              value={{
+                  light: lightTheme.className,
+                  dark: darkTheme.className,
+              }}>
+              <NextUIProvider>
+                  <Head>
+                      <link
+                          rel="alternate"
+                          hrefLang={translations.alternate_lang}
+                          href={translations.alternate_url}
+                      />
+                  </Head>
+                  <PlausibleProvider domain={translations.plausible}>
+                      <Provider store={store}>
+                          <>
+                              <Pwa />
 
-                <Gate>
-                  <SWRConfig
-                    value={{
-                      revalidateOnFocus: false,
-                      fetcher: ({ query, variables }) =>
-                        request("/api/graphql", query, variables),
-                    }}>
-                    <Component {...pageProps} />
-                  </SWRConfig>
-                </Gate>
-              </>
-            </Provider>
-          </PlausibleProvider>
-        </NextUIProvider>
-      </NextThemesProvider>
-    </LazyMotion>
+                              <PersistGate persistor={persistor}>
+                                  <SWRConfig
+                                      value={{
+                                          revalidateOnFocus: false,
+                                          fetcher: ({ query, variables }) =>
+                                              request(
+                                                  "/api/graphql",
+                                                  query,
+                                                  variables
+                                              ),
+                                      }}>
+                                      <Component {...pageProps} />
+                                  </SWRConfig>
+                              </PersistGate>
+                          </>
+                      </Provider>
+                  </PlausibleProvider>
+              </NextUIProvider>
+          </NextThemesProvider>
+      </LazyMotion>
   );
 }
 
