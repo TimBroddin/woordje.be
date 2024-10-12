2814c661048903817038d28211cc7ec0e3cdb1f1
Tim Broddin <tim@wannabes.be>
2024-10-12 18:10:41 +0200

    Don't proxy

--- next.config.js
+++ next.config.js
@@ -6,50 +6,48 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withBundleAnalyzer(
-    withPWA(
-        withPlausibleProxy()({
-            reactStrictMode: true,
-            images: {
-                domains: ["media.giphy.com"],
-            },
-            i18n: {
-                locales: ["nl-NL", "nl-BE"],
-                defaultLocale: "nl-BE",
-                localeDetection: false,
+  withPWA({
+    reactStrictMode: true,
+    images: {
+      domains: ["media.giphy.com"],
+    },
+    i18n: {
+      locales: ["nl-NL", "nl-BE"],
+      defaultLocale: "nl-BE",
+      localeDetection: false,
 
-                domains: [
-                    {
-                        domain: "www.woordje.be",
-                        defaultLocale: "nl-BE",
-                    },
-                    {
-                        domain: "www.woordol.nl",
-                        defaultLocale: "nl-NL",
-                    },
-                ],
-            },
-            async rewrites() {
-                return [
-                    {
-                        source: "/",
-                        destination: "/speel/6",
-                    },
-                ];
-            },
-            swcMinify: true,
+      domains: [
+        {
+          domain: "www.woordje.be",
+          defaultLocale: "nl-BE",
+        },
+        {
+          domain: "www.woordol.nl",
+          defaultLocale: "nl-NL",
+        },
+      ],
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/",
+          destination: "/speel/6",
+        },
+      ];
+    },
+    swcMinify: true,
 
-            pwa: {
-                dest: "public",
-                register: false,
-                skipWaiting: false,
-                mode: "production",
-                runtimeCaching,
-                buildExcludes: [
-                    /middleware-manifest\.json$/,
-                    /_middleware.js$/,
-                    /_middleware.js.map$/,
-                ],
-            },
-        })
-    )
+    pwa: {
+      dest: "public",
+      register: false,
+      skipWaiting: false,
+      mode: "production",
+      runtimeCaching,
+      buildExcludes: [
+        /middleware-manifest\.json$/,
+        /_middleware.js$/,
+        /_middleware.js.map$/,
+      ],
+    },
+  })
 );
