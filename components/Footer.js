9b208ea3e6e2a88a73c6ea1505284281e25b1ba9
Tim Broddin <tim@broddin.be>
2025-10-11 16:31:26 +0200

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -273,11 +273,11 @@ const Footer = ({ gameId }) => {
                                       color: "$white",
                                       textDecoration: "underline",
                                   }}
-                                  href="https://broddin.be/"
+                                  href="https://www.titansofindustry.be/"
                                   style={{ textDecoration: "underline" }}
                                   rel="noreferrer"
                                   target="_blank">
-                                  Tim&nbsp;Broddin
+                                  Titans of Industry
                               </Link>
                               .
                           </Text>
@@ -299,25 +299,6 @@ const Footer = ({ gameId }) => {
                   .
               </Text>
           </Text>
-
-        <div className="m-8 mt-16 pb-8 text-gray-700 bg-blue-50 border border-blue-200 rounded-md p-4 shadow-md">
-            <div className="flex items-center mb-2">
-            <svg className="w-5 h-5 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-            </svg>
-            <span className="font-semibold text-blue-700">Tip</span>
-            </div>
-            Maak eenvoudig verlanglijstjes voor elke gelegenheid! Probeer{" "}
-            <Link
-            href={`https://lijsje.be/?utm_source=${translations.plausible}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            css={{ color: "$primary" }}
-            >
-            Lijsje.be
-            </Link>{" "}
-            - de perfecte tool voor het organiseren van je verlanglijsten. Van de maker van Woordje!
-        </div>
       </Container>
   );
 };
